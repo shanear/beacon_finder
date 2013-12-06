4f7985687d2f599540898e8a57270d8768be9a46
Alexandra <aprice@thoughtworks.com>
2013-12-05 17:02:09 -0800

    some changes to the messages

---
+++ BeaconFinderTests/ClueViewControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  ClueViewControllerTests.m
+//  BeaconFinder
+//
+//  Created by aprice on 12/5/13.
+//  Copyright (c) 2013 thoughtworks. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ClueViewControllerTests : XCTestCase
+
+@end
+
+@implementation ClueViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
